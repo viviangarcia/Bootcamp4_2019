4ba43868c241bf53bcce300aa53287311f445c9a
Vivian Garcia <garcviv@ufl.edu>
2019-11-05 23:48:44 -0500

    filters building

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import data from './data/data';
-
-//The app is now passing the data as a property
-ReactDOM.render(<App data={data} />, document.getElementById('root'));
